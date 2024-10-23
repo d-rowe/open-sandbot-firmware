76aae477e4abbad6fdced5360f2037b279f51f29
Daniel Rowe <danielrowetech@gmail.com>
2024-10-22 21:41:20 -0700

    Split arm worker to separate task

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use arm::Arm;
 use coordinate::PolarCoordinate;
 use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, channel::Channel};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -12,25 +13,32 @@ mod coordinate;
 mod stepper;
 mod stepper_pair;
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+static POSITION_CHANNEL: Channel<ThreadModeRawMutex, &PolarCoordinate, 10> = Channel::new();
+
+#[embassy_executor::task]
+async fn arm_task() {
     let mut arm = Arm::new();
     loop {
-        arm.move_to(&PolarCoordinate {
+        let coordinate: &PolarCoordinate = POSITION_CHANNEL.receive().await;
+        arm.move_to(coordinate).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    spawner.spawn(arm_task()).unwrap();
+
+    Timer::after_secs(5).await;
+    POSITION_CHANNEL
+        .send(&PolarCoordinate {
             theta: 0.0,
             rho: 1.0,
         })
         .await;
-        arm.move_to(&PolarCoordinate {
-            theta: 1.57,
-            rho: 0.5,
-        })
-        .await;
-        arm.move_to(&PolarCoordinate {
+    POSITION_CHANNEL
+        .send(&PolarCoordinate {
             theta: 0.0,
             rho: 0.0,
         })
         .await;
-        Timer::after_secs(5).await;
-    }
 }
